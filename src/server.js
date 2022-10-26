98fd8aae5743da668eb724b8e65ef1f12047ea97
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-10-27 00:21:51 +0200

    added routes

---
+++ src/server.js
@@ -0,0 +1,77 @@
+import express from "express";
+
+let articlesInfo = [{
+    name: 'elite-capture',
+    upvotes: 0,
+    comments: [],
+},
+{
+    name: 'overton-window',
+    upvotes: 0,
+    comments: [],
+},
+{
+    name: 'normalcy-bias',
+    upvotes: 0,
+    comments: [],
+},
+{
+    name: 'false-flag',
+    upvotes: 0,
+    comments: [],
+},
+{
+    name: 'rabbit-holes',
+    upvotes: 0,
+    comments: [],
+}]
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+const { name } = req.params;
+const article = articlesInfo.find(a => a.name === name);
+if (article) {
+article.upvotes += 1;
+res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+}else {
+    res.send('That article does not exist')
+}
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {name} = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+    article.comments.push({ postedBy, text});
+    res.send(article.comments);
+    } else {
+        res.send('That article does not exist')
+    }
+});
+
+
+
+
+
+// app.get("/hello", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get('/hello/:name', (req, res) => {
+//     const name = req.params.name;
+//     //or const { name } = req.params;
+//     res.send(`hello again ${name}.`);
+// })
+
+
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+
+//   res.send(`hello, ${req.body.name}`);
+// });
+
+app.listen(8000, () => {
+  console.log("server is listening on port 8000");
+});
