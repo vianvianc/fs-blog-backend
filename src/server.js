b58537cd7cf00dfbaf63dd4eeca3b435777c696f
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-11-11 19:01:44 +0100

    server

--- src/server.js
+++ src/server.js
@@ -1,64 +1,62 @@
 import express from "express";
-import {MongoClient} from 'mongodb'
+import { MongoClient } from "mongodb";
 
-let articlesInfo = [{
-    name: 'elite-capture',
+let articlesInfo = [
+  {
+    name: "elite-capture",
     upvotes: 0,
     comments: [],
-},
-{
-    name: 'overton-window',
+  },
+  {
+    name: "overton-window",
     upvotes: 0,
     comments: [],
-},
-{
-    name: 'normalcy-bias',
+  },
+  {
+    name: "normalcy-bias",
     upvotes: 0,
     comments: [],
-},
-{
-    name: 'false-flag',
+  },
+  {
+    name: "false-flag",
     upvotes: 0,
     comments: [],
-},
-{
-    name: 'rabbit-holes',
+  },
+  {
+    name: "rabbit-holes",
     upvotes: 0,
     comments: [],
-}]
+  },
+];
 const app = express();
 app.use(express.json());
 
-app.get('/api/articles/:name', (req,res) => {
-   const {name} = req.params; 
-})
-
-app.put('/api/articles/:name/upvote', (req, res) => {
-const { name } = req.params;
-const article = articlesInfo.find(a => a.name === name);
-if (article) {
-article.upvotes += 1;
-res.send(`The ${name} article now has ${article.upvotes} upvotes`);
-}else {
-    res.send('That article does not exist')
-}
+app.get("/api/articles/:name", (req, res) => {
+  const { name } = req.params;
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
-    const {name} = req.params;
-    const { postedBy, text } = req.body;
-    const article = articlesInfo.find(a => a.name === name);
-    if (article) {
-    article.comments.push({ postedBy, text});
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+  } else {
+    res.send("That article does not exist");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
     res.send(article.comments);
-    } else {
-        res.send('That article does not exist')
-    }
+  } else {
+    res.send("That article does not exist");
+  }
 });
-
-
-
-
 
 // app.get("/hello", (req, res) => {
 //   res.send("Hello!");
@@ -69,7 +67,6 @@ app.post('/api/articles/:name/comments', (req, res) => {
 //     //or const { name } = req.params;
 //     res.send(`hello again ${name}.`);
 // })
-
 
 // app.post("/", (req, res) => {
 //   console.log(req.body);
