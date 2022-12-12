04123e7d2ef251a70d2eda52069a2ae65450678f
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-12-12 22:25:13 +0100

    server changes

--- src/server.js
+++ src/server.js
@@ -45,6 +45,9 @@ app.use(express.json());
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
   
+
+
+  
   const client = new MongoClient('mongodb://127.0.0.1:27017');
   await client.connect();
   const db = client.db('react-blog-db');
